3010860d24418e47b7be1d852105932a57c2d6d0
Andre <andriej2301@gmail.com>
2024-02-20 15:39:12 +0100

    Initial commit.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = input('Enter string:')
-print(f"The string is of type: {type(x)}\n"
-      f"Minimal ASCII code: {min(x)}\n"
-      f"Maximal ASCII code: {max(x)}\n"
-      f"Length of the string: {len(x)}")
+if __name__ == '__main__':
+      x = input('Enter string:')
+      print(f"The string is of type: {type(x)}\n"
+            f"Minimal ASCII code: {min(x)}\n"
+            f"Maximal ASCII code: {max(x)}\n"
+            f"Length of the string: {len(x)}")
