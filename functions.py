ddd124c3a8d08fdce1634d08b0b3d92f82e483fb
Andre <andriej2301@gmail.com>
2024-02-20 17:17:58 +0100

    feature: Added ability for measuring length of words in list.

--- functions.py
+++ functions.py
@@ -4,14 +4,65 @@ global global_variable
 global_variable = 31
 
 
+def predefined_functions_demo():
+    x = input('Enter string:')
+    print(f"The string is of type: {type(x)}\n"
+          f"Minimal ASCII code: {min(x)}\n"
+          f"Maximal ASCII code: {max(x)}\n"
+          f"Length of the string: {len(x)}")
+
+    text = "merito gdansk 4ever"
+    text_match = regex_demo(text)
+    print(f"The text is: {text}")
+    if text_match:
+        print("Match found!")
+    else:
+        print("Match not found!")
+
+
 def regex_demo(text):
     pattern = "^merito.*4ever"
     searching_result = re.search(pattern, text)
     return searching_result
 
 
-def global_variable_demo():
+def global_variable_demo(additional=0):
     local_variable = 32
 
     print(f"This is value of the global variable: {global_variable}")
     print(f"This is value of the local variable: {local_variable}")
+    print(additional)
+
+
+def function_as_argument(function, argument):
+    return function(argument)
+
+
+def return_even_numbers(numbers_array):
+    even_numbers = []
+    for number in numbers_array:
+        if number % 2 == 0:
+            even_numbers.append(number)
+    return even_numbers
+
+
+def filter_demo(word_list):
+    starting_with_a = filter(check_if_word_starts_with_lettter, word_list)
+    return starting_with_a
+
+
+def check_if_word_starts_with_lettter(word_list):
+    if word_list[0] == 'a':
+        return True
+    return False
+
+def square_power(number):
+    return number ** 2
+
+def add_5(number):
+    return number + 5
+
+def apply_functions(function_list, value):
+    value = function_list[0](value)
+    value = function_list[1](value)
+    return value
--- main.py
+++ main.py
@@ -1,18 +1,44 @@
-from functions import regex_demo, global_variable_demo
+import math
+from functions import regex_demo, global_variable_demo, return_even_numbers, predefined_functions_demo, \
+    function_as_argument, filter_demo, square_power, add_5, apply_functions
 
 if __name__ == '__main__':
-    x = input('Enter string:')
-    print(f"The string is of type: {type(x)}\n"
-          f"Minimal ASCII code: {min(x)}\n"
-          f"Maximal ASCII code: {max(x)}\n"
-          f"Length of the string: {len(x)}")
-
-    text = "merito gdansk 4ever"
-    text_match = regex_demo(text)
-    print(f"The text is: {text}")
-    if text_match:
-        print("Match found!")
-    else:
-        print("Match not found!")
-
+    predefined_functions_demo()
     global_variable_demo()
+    print("\nNow the function has been passed as an argument!")
+    function_as_argument(global_variable_demo, 58)
+
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    even_numbers = return_even_numbers(numbers)
+    print("\nEven numbers:")
+    for even_number in even_numbers:
+        print(even_number)
+
+    word_list = ["admin", "debug", "python", "asembler"]
+    filtered_word_list = filter_demo(word_list)
+    print("\nWords starting with a:")
+    for word in filtered_word_list:
+        print(word)
+
+    print("Powers of numbers:")
+    numbers_pow2 = map(lambda x: pow(x, 2), numbers)
+    for number in numbers_pow2:
+        print(number)
+
+    function_list = [square_power, add_5]
+    altered_value = apply_functions(function_list, 8)
+    print(f"\nAltered value: {altered_value}")
+
+    square_numbers = [pow(x, 2) for x in range(1, 11)]
+    print("\nSquare numbers:")
+    for number in square_numbers:
+        print(number)
+
+    print("\nWord list:")
+    for word in word_list:
+        print(word)
+
+    words_lenghts = [ len(word) for word in word_list ]
+    print("\nLengths of these words:")
+    for length in words_lenghts:
+        print(length)
